cce635085404eedef7e9e04ce2aa83bdacdcd704
David Ma <david.ma@petermac.org>
2018-06-04 16:01:37 +1000

    Delete unused stuff, use less confusing layout

--- config/express.js
+++ config/express.js
@@ -2,6 +2,8 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let path = require('path');
 
+console.log("serving public to /gallery");
+
 module.exports = function() {
 	let app = express();
 	
@@ -13,7 +15,7 @@ module.exports = function() {
 	app.use('/gallery', express.static('public'));
 
 	app.get('/gallery', function(req,res) {
-		res.sendFile(path.join(__dirname + '/../index.html'));
+		res.sendFile(path.join(__dirname + '/index.html'));
 	});
 
 	return app;
