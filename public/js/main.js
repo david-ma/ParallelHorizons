d37fd3e43170d0473da6d83b37fcd07e616892b1
David Ma <david.ma@petermac.org>
2020-09-10 17:12:49 +1000

    Add a statue to the gallery

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 import * as THREE from '/gallery/js/three.module.js';
-// import { GLTFLoader } from '/gallery/js/GLTFLoader.js';
 import { PointerLockControls } from '/gallery/js/PointerLockControls.js';
 
 var lastCalledTime;
@@ -586,69 +585,6 @@ else {
                     img.map.needsUpdate = true; //ADDED
                 }(i))
             }
-
-
-
-
-// Add a sculpture?
-
-
-
-            // // Instantiate a loader
-            // var loader = new GLTFLoader();
-
-            // // Optional: Provide a DRACOLoader instance to decode compressed mesh data
-            // // var dracoLoader = new DRACOLoader();
-            // // dracoLoader.setDecoderPath('/examples/js/libs/draco/');
-            // // loader.setDRACOLoader(dracoLoader);
-
-            // // Load a glTF resource
-            // loader.load(
-            //     // resource URL
-            //     '/gallery/3d/cesar/scene.gltf',
-            //     // called when the resource is loaded
-            //     function (gltf) {
-            //         console.log("Ok, drawing the gltf now");
-            //         console.log(gltf);
-
-            //         gal.scene.add(gltf.scene);
-
-            //         gltf.animations; // Array<THREE.AnimationClip>
-            //         gltf.scene; // THREE.Group
-            //         gltf.scenes; // Array<THREE.Group>
-            //         gltf.cameras; // Array<THREE.Camera>
-            //         gltf.asset; // Object
-
-            //     },
-            //     // called while loading is progressing
-            //     function (xhr) {
-
-            //         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-            //     },
-            //     // called when loading has errors
-            //     function (error) {
-
-            //         console.log('An error happened');
-            //         console.log(error);
-
-            //     }
-            // );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         },
         render: function () {
             framerate();
