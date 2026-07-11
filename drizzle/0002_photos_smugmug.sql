38682a4daf59cecbefb2f2d3e1cc286481b0c2a6
David Ma <hello@david-ma.net>
2026-07-11 21:20:59 +1000

    Add migrations

---
+++ drizzle/0002_photos_smugmug.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `photos` ADD `smugmug_album_key` varchar(255);--> statement-breakpoint
+ALTER TABLE `photos` ADD `smugmug_image_key` varchar(255);--> statement-breakpoint
+ALTER TABLE `photos` ADD `adapter_name` varchar(255);--> statement-breakpoint
+ALTER TABLE `photos` ADD `archived_md5` varchar(255);
